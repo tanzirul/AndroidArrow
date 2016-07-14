beb87f63aa26da013039574c48829085f181c510
Tanzirul Azim <tanzirul@users.noreply.github.com>
2016-07-14 01:08:21 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ APK=$1
 aapt list -a $APK | grep SdkVersion >> sdk.txt
 java -jar converter.jar sdk.txt
 chmod 777 drive.sh
-./drive.sh  $APK Lorg/hermit/android/core/AppUtils$Version getAppVersion
+./drive.sh  $APK targetClassName targetMethodName
