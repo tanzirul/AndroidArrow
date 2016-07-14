660bbf987eb302a53689648d0a5f8457b548747a
Tanzirul Azim <tanzirul@users.noreply.github.com>
2016-07-14 01:09:17 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 
 APK=$1
+TARGET_CLASS=$2
+TARGET_METHOD=$3
 aapt list -a $APK | grep SdkVersion >> sdk.txt
 java -jar converter.jar sdk.txt
 chmod 777 drive.sh
-./drive.sh  $APK targetClassName targetMethodName
+./drive.sh  $APK $TARGET_CLASS $TARGET_METHOD
